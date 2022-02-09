3c9c50240cac054178ae18f17ba112e5f44413f7
Nathan Abourbih <nathan@abourbih.com>
2022-02-09 08:53:00 -0500

    Initial commit

---
+++ JobManager/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using JobManager.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace JobManager.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
