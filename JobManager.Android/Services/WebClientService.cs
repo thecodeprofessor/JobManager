de30d15bd3cca850c1b7f00ba84c41688b0463aa
Nathan Abourbih <nathan@abourbih.com>
2022-02-16 11:16:47 -0500

    Added WebClientService
    Added a web client service interface and implemented it in Android.

---
+++ JobManager.Android/Services/WebClientService.cs
@@ -0,0 +1,34 @@
+﻿    using System.IO;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Android.Content;
+    using JobManager.Droid.Services;
+    using JobManager.Services;
+    using Xamarin.Forms;
+
+[assembly: Dependency(typeof(WebClientService))]
+namespace JobManager.Droid.Services
+    {
+        public class WebClientService : IWebClientService
+        {
+
+            //Related Documentation:
+            //https://docs.microsoft.com/en-us/xamarin/xamarin-forms/data-cloud/web-services/rest
+
+            public async Task<string> GetString(string Uri)
+            {
+                try
+                {
+                    HttpClient client;
+                    client = new HttpClient();
+
+                    HttpResponseMessage response = await client.GetAsync(Uri);
+                    return response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync() : null;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+    }
---
+++ JobManager/Services/IWebClientService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobManager.Services
+{
+    public interface IWebClientService
+    {
+        //Related Documentation:
+        //https://docs.microsoft.com/en-us/xamarin/xamarin-forms/app-fundamentals/dependency-service/introduction
+        //https://github.com/xamarin/xamarin-forms-samples/tree/main/DependencyService
+
+        Task<string> GetString(string Uri);
+    }
+}
