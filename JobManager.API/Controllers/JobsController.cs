73e0cfb39e47fbb133d021052019ee87086b3f16
Nathan Abourbih <nathan@abourbih.com>
2022-02-16 11:29:39 -0500

    Added JobsController
    Added a jobs controller and implemented a GET method for retrieving all default jobs.

---
+++ JobManager.API/Controllers/JobsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using JobManager.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobManager.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobsController : ControllerBase
+    {
+        private readonly ILogger<JobsController> _logger;
+
+        public JobsController(ILogger<JobsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Job> Get()
+        {
+            return GetDefaultJobs();
+        }
+
+        private List<Job> GetDefaultJobs()
+        {
+            var jobs = new List<Job>()
+            {
+                new Job { Id = 1, Name = "Job A API", Description = "This is job a." },
+                new Job { Id = 2, Name = "Job B API", Description = "This is job b." },
+                new Job { Id = 3, Name = "Job C API", Description = "This is job c." },
+                new Job { Id = 4, Name = "Job D API", Description = "This is job d." }
+            };
+
+            return jobs;
+        }
+    }
+}
