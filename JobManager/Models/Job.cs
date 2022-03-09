3eea57273e8b9554227f4a3221192ae24ac1f08f
Nathan Abourbih <nathan@abourbih.com>
2022-03-08 23:10:35 -0500

    Fix Jobs Model Mistake
    Removed a database line from the shared head project model that was included by mistake.

--- JobManager/Models/Job.cs
+++ JobManager/Models/Job.cs
@@ -6,7 +6,6 @@ namespace JobManager.Models
 {
     public class Job
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
